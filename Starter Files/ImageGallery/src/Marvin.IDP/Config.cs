e2f09f1409177c678d644a4bcfe4a82953288a85
Michael Alan Hendricks <michaelh@lawforall.co.za>
2018-11-19 20:44:40 +0200

    Implementing Identity Server 4

---
+++ Starter Files/ImageGallery/src/Marvin.IDP/Config.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+using IdentityServer4.Test;
+using IdentityServer4.Models;
+
+namespace Marvin.IDP
+{
+    public static class Config
+    {
+        // test users
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "db34534-3345j34-34534-sadfasdfie4",
+                    Username = "Frank",
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Frank"),
+                        new Claim("family_name", "Underwood")
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "ds34j4-jkdfi23f=32jsa3k9-asxfdsaer23",
+                    Username = "Claire", 
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Claire"),
+                        new Claim("family_name", "Underwood")
+                    }
+                }
+            };
+        }
+
+        // identity-related resources (scopes)
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>();
+        }
+    }
+}
